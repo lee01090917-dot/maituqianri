a01a7a07618f3b35663d9de55629d3712dff15e4
lee01090917-dot <lee01090917@gmail.com>
2026-08-01 01:22:50 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,14 @@
 import {
     auth,
     provider,
+    db,
     signInWithPopup
 } from "./firebase.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const loginButton = document.getElementById("google-login");
 
@@ -10,14 +16,49 @@ loginButton.addEventListener("click", async () => {
 
     try {
 
-        const result = await signInWithPopup(
-            auth,
-            provider
-        );
+        const result = await signInWithPopup(auth, provider);
 
-        console.log(result.user);
+        const user = result.user;
 
-        alert(`歡迎 ${result.user.displayName}`);
+        const docRef = doc(db, "members", user.uid);
+
+        const docSnap = await getDoc(docRef);
+
+        if (!docSnap.exists()) {
+
+            location.href = "register.html";
+
+            return;
+
+        }
+
+        const data = docSnap.data();
+
+        switch (data.status) {
+
+            case "已通過":
+                location.href = "app.html";
+                break;
+
+            case "待審核":
+                alert("您的帳號正在等待管理員審核。");
+                break;
+
+            case "保留":
+                alert("您的帳號目前需要補充資料。");
+                break;
+
+            case "已拒絕":
+                alert("您的申請未通過，請聯繫神燈精靈。");
+                break;
+
+            case "已停權":
+                alert("您的帳號已停權。");
+                break;
+
+            default:
+                alert("未知狀態");
+        }
 
     } catch (error) {
 
