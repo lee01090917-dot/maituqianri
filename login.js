bab6aeac99dea53eb27a59452bb318fa1a72a0f3
lee01090917-dot <lee01090917@gmail.com>
2026-08-01 01:10:32 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,30 @@
-console.log("login.js 已載入");
+import {
+    auth,
+    provider,
+    signInWithPopup
+} from "./firebase.js";
+
+const loginButton = document.getElementById("google-login");
+
+loginButton.addEventListener("click", async () => {
+
+    try {
+
+        const result = await signInWithPopup(
+            auth,
+            provider
+        );
+
+        console.log(result.user);
+
+        alert(`歡迎 ${result.user.displayName}`);
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert("登入失敗");
+
+    }
+
+});
